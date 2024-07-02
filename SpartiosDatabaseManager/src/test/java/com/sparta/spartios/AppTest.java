60abf8722fb868fce202f122b0ea523e351b9a9b
Katherine Carver-Hardy <katabeth.ch@gmail.com>
2024-07-02 18:36:53 +0100

    Added test package and file

---
+++ SpartiosDatabaseManager/src/test/java/com/sparta/spartios/AppTest.java
@@ -0,0 +1,13 @@
+package com.sparta.spartios;
+
+import com.sparta.spartios.logging.LoggerInitialiser;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppTest {
+    final static Logger logger = LoggerInitialiser.getLogger(Level.ALL, Level.FINE, false, false);
+
+
+    
+}
