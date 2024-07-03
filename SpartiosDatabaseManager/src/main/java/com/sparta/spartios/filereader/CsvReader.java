7ef9e6d437e12dfad6034d06561ac49e93c16189
Chris Rayner <crayner@spartaglobal.com>
2024-07-03 07:16:20 +0100

    Init dtos and filereader packages init Employee and CsvReader Classes

---
+++ SpartiosDatabaseManager/src/main/java/com/sparta/spartios/filereader/CsvReader.java
@@ -0,0 +1,32 @@
+package com.sparta.spartios.filereader;
+
+import com.sparta.spartios.logging.LoggerInitialiser;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.logging.*;
+
+public class CsvReader{
+    final static Logger logger = LoggerInitialiser.getLogger(Level.ALL, Level.FINE, false, false);
+
+    public static HashSet<String> getAllLines(){
+        HashSet<String> lines = new HashSet<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/employees-corrupted(in).csv"))){
+            logger.log(Level.FINE, "Init file read");
+            String line;
+            while((line = bufferedReader.readLine())!= null){
+                logger.log(Level.FINER, "Added line: " +line + "\nTo Hash Set");
+                lines.add(line);
+            }
+            return lines;
+        }
+        catch (IOException e){
+            logger.log(Level.WARNING, "File to read not found");
+        }
+        finally {
+            return lines;
+        }
+    }
+}
