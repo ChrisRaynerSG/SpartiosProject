15d97afe5cc6f928f1ccd0052178dba7f70c730f
Katherine <katabeth.ch@gmail.com>
2024-07-03 16:12:06 +0100

    Cleaned up testing lines and files

--- SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
+++ SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
@@ -5,13 +5,9 @@ import com.sparta.spartios.logging.LoggerInitialiser;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static com.sparta.spartios.App2.loggerCheck;
-
 public class App {
     final static Logger logger = LoggerInitialiser.getLogger(Level.ALL, true);
     public static void main(String[] args) {
-        logger.finest("test string");
-        loggerCheck();
-        logger.finest("finest string");
+
     }
 }
