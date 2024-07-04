5442bb71cf0ee7b45f6f6bb1b6ceb5fa22f065ad
Chris Rayner <crayner@spartaglobal.com>
2024-07-04 20:21:36 +0100

    Added methods to main to output program results

--- SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
+++ SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
@@ -1,10 +1,12 @@
 package com.sparta.spartios;
 
 
+import com.sparta.spartios.Dao.EmployeeDAO;
 import com.sparta.spartios.dbmanagement.ConnectToDatabase;
 import com.sparta.spartios.dbmanagement.DatabaseInitialiser;
 import com.sparta.spartios.logging.LoggerInitialiser;
 
+import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,7 +14,17 @@ public class App {
     public final static Logger logger = LoggerInitialiser.getLogger(Level.INFO, true);
     public static void main(String[] args) {
 
-        DatabaseInitialiser.instantiateDatabase(ConnectToDatabase.employeeDatabaseConnection(), true);
+        EmployeeDAO accessEmployees = new EmployeeDAO();
+//
+//        System.out.println(accessEmployees.getEmployees());
+
+        System.out.println(accessEmployees.getEmployee("179414").toString());
+//
+//        System.out.println(accessEmployees.getEmployees(LocalDate.of(2001,1,1),LocalDate.of(2003,1,1)));
+//
+//        System.out.println(accessEmployees.getEmployees(22, 30));
+//
+//        System.out.println(accessEmployees.getEmployees("sm"));
 
     }
 }
