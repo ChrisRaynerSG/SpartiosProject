925e28113824949491fecee2bf1f7c53459c87d4
Chris Rayner <crayner@spartaglobal.com>
2024-07-04 21:06:50 +0100

    updated App to include Input Validation provided by InputValidation class

--- SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
+++ SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
@@ -1,12 +1,14 @@
 package com.sparta.spartios;
 
-
 import com.sparta.spartios.Dao.EmployeeDAO;
-import com.sparta.spartios.dbmanagement.ConnectToDatabase;
-import com.sparta.spartios.dbmanagement.DatabaseInitialiser;
+
+import com.sparta.spartios.datasanitisation.InputValidation;
+
+import com.sparta.spartios.dtos.Employee;
 import com.sparta.spartios.logging.LoggerInitialiser;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,16 +17,60 @@ public class App {
     public static void main(String[] args) {
 
         EmployeeDAO accessEmployees = new EmployeeDAO();
-//
-//        System.out.println(accessEmployees.getEmployees());
 
-        System.out.println(accessEmployees.getEmployee("179414").toString());
-//
-//        System.out.println(accessEmployees.getEmployees(LocalDate.of(2001,1,1),LocalDate.of(2003,1,1)));
-//
-//        System.out.println(accessEmployees.getEmployees(22, 30));
-//
-//        System.out.println(accessEmployees.getEmployees("sm"));
+        HashSet<Employee> consoleOutput = new HashSet<>();
 
+        String searchByID = "179414";
+        String searchByName = "Smith";
+        String searchByAgeLower = "22";
+        String searchByAgeUpper = "45";
+        String joinDateLower = "01/01/1992"; //MM/dd/yyyy
+        String joinDateUpper = "01/01/2007"; //MM/dd/yyyy
+        String invalidInput = "Invalid Input!";
+
+
+//        if(InputValidation.hasValidID(searchByID)){
+//            System.out.println(accessEmployees.getEmployee(searchByID).toString());
+//        }
+
+        if(InputValidation.hasValidPartialName(searchByName)){
+            consoleOutput = accessEmployees.getEmployees(searchByName);
+            printResultsToConsole(consoleOutput);
+        }
+
+        else{
+            System.out.println(invalidInput);
+        }
+
+        if(InputValidation.hasValidAgeRange(searchByAgeLower,searchByAgeUpper)){
+            consoleOutput = accessEmployees
+                    .getEmployees(Integer.parseInt(searchByAgeLower),
+                        Integer.parseInt(searchByAgeUpper));
+            printResultsToConsole(consoleOutput);
+
+        }
+
+        else{
+            System.out.println(invalidInput);
+        }
+
+        if(InputValidation.hasValidDates(joinDateLower, joinDateUpper)){
+            LocalDate date1 = LocalDate.parse(joinDateLower, InputValidation.formatInputDates());
+            LocalDate date2 = LocalDate.parse(joinDateUpper, InputValidation.formatInputDates());
+
+            consoleOutput = accessEmployees.getEmployees(date1,date2);
+            printResultsToConsole(consoleOutput);
+        }
+        else{
+            System.out.println(invalidInput);
+        }
+
+//        System.out.println(DataCheck.getNumberOfCorruptions());
+    }
+
+    private static void printResultsToConsole(HashSet<Employee> consoleOutput) {
+        for(Employee employee : consoleOutput){
+            System.out.println(employee.toString()+"\n");
+        }
     }
 }
