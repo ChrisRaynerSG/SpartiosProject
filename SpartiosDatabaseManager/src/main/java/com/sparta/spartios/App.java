1078f447c464f3acc84e715b2799a4bce37df515
Chris Rayner <crayner@spartaglobal.com>
2024-07-04 15:33:26 +0100

    changed DoB and DoJ to be imported as dates into database and formatted them so they work

--- SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
+++ SpartiosDatabaseManager/src/main/java/com/sparta/spartios/App.java
@@ -1,5 +1,8 @@
 package com.sparta.spartios;
 
+
+import com.sparta.spartios.dbmanagement.ConnectToDatabase;
+import com.sparta.spartios.dbmanagement.DatabaseInitialiser;
 import com.sparta.spartios.logging.LoggerInitialiser;
 
 import java.util.logging.Level;
@@ -8,5 +11,8 @@ import java.util.logging.Logger;
 public class App {
     public final static Logger logger = LoggerInitialiser.getLogger(Level.INFO, true);
     public static void main(String[] args) {
+
+        DatabaseInitialiser.instantiateDatabase(ConnectToDatabase.employeeDatabaseConnection(), true);
+
     }
 }
