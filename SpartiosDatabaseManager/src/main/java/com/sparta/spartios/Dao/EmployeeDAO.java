c5411dc1cffd7330b0deb77e0ba5c4d9c7d2c565
Gilbert Reid <greid@spartaglobal.com>
2024-07-05 09:22:32 +0100

    added some logging to dao and db querier

--- SpartiosDatabaseManager/src/main/java/com/sparta/spartios/Dao/EmployeeDAO.java
+++ SpartiosDatabaseManager/src/main/java/com/sparta/spartios/Dao/EmployeeDAO.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import static com.sparta.spartios.App.logger;
 import static com.sparta.spartios.Dao.QueryBuilder.*;
 import static com.sparta.spartios.Dao.QueryOptions.*;
 
@@ -16,11 +17,13 @@ public class EmployeeDAO implements DAO {
 
     @Override
     public HashSet<Employee> getEmployees() {
+        logger.fine("In getEmployees");
         return db.queryDB(get(EVERYTHING, from(EMPLOYEES)));
     }
 
     @Override
     public Employee getEmployee(String EmployeeID) {
+        logger.fine("In getEmployee");
         Iterator<Employee> employeeFound = db.queryDB(get(EVERYTHING,from(EMPLOYEES,where(EMPLOYEE_ID,isEqualTo(EmployeeID))))).iterator();
         if(employeeFound.hasNext()){
             return employeeFound.next();
@@ -32,16 +35,19 @@ public class EmployeeDAO implements DAO {
 
     @Override
     public HashSet<Employee> getEmployees(String lastName) {
+        logger.fine("In getEmployees");
         return db.queryDB(get(EVERYTHING,from(EMPLOYEES,where(LAST_NAME,isLike(lastName)))));
     }
 
     @Override
     public HashSet<Employee> getEmployees(LocalDate joinedAfter, LocalDate joinedBefore) {
+        logger.fine("In getEmployees");
         return db.queryDB(get(EVERYTHING,from(EMPLOYEES,where(DATE_OF_JOINING,isBetween(joinedAfter.toString(),joinedBefore.toString())))));
     }
 
     @Override
     public HashSet<Employee> getEmployees(Integer olderThan, Integer youngerThan) {
+        logger.fine("In getEmployees");
         LocalDate bornBefore = LocalDate.now().minusYears(olderThan);
         LocalDate bornAfter = LocalDate.now().minusYears(youngerThan);
         return db.queryDB(get(EVERYTHING,from(EMPLOYEES,where(DATE_OF_BIRTH,isBetween(bornAfter.toString(),bornBefore.toString())))));
@@ -49,6 +55,7 @@ public class EmployeeDAO implements DAO {
 
     @Override
     public void deleteEmployee(String EmployeeID) {
+        logger.fine("In deleteEmployee");
         db.deleteFromDB(deleteFrom(EMPLOYEES,where(EMPLOYEE_ID,isEqualTo(EmployeeID))));
     }
 
